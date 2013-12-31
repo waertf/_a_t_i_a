fbe62630a9909c7c452ecb943b4ce27f470148cd
nb <waertf.register@m2k.com.tw>
2013-12-31 15:26:45 +0800

    add sqlTest project

---
+++ ConsoleApplication1SqlTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApplication1SqlTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlClient sql_client = new SqlClient(ConfigurationManager.AppSettings["SQL_SERVER_IP"], ConfigurationManager.AppSettings["SQL_SERVER_PORT"], ConfigurationManager.AppSettings["SQL_SERVER_USER_ID"], ConfigurationManager.AppSettings["SQL_SERVER_PASSWORD"], ConfigurationManager.AppSettings["SQL_SERVER_DATABASE"], ConfigurationManager.AppSettings["Pooling"], ConfigurationManager.AppSettings["MinPoolSize"], ConfigurationManager.AppSettings["MaxPoolSize"], ConfigurationManager.AppSettings["ConnectionLifetime"]);
+            while (!sql_client.connect())
+            {
+            }
+            
+            Console.WriteLine("press any key to exist...");
+            Console.Read();
+
+        }
+    }
+}
---
+++ ConsoleApplication1SqlTest/SqlClient.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Devart.Data.PostgreSql;
+using System.Data;
+using log4net;
+using log4net.Config;
+
+namespace ConsoleApplication1SqlTest
+{
+    class SqlClient
+    {
+        PgSqlConnectionStringBuilder pgCSB = new PgSqlConnectionStringBuilder();
+        PgSqlConnection pgSqlConnection;
+        public bool IsConnected{get;set;}
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        public SqlClient(string ip, string port, string user_id, string password, string database, string Pooling, string MinPoolSize, string MaxPoolSize, string ConnectionLifetime)
+        {
+            pgCSB.Host = ip;
+            pgCSB.Port = int.Parse(port);
+            pgCSB.UserId = user_id;
+            pgCSB.Password = password;
+            pgCSB.Database = database;
+
+            pgCSB.Pooling = bool.Parse(Pooling);
+            pgCSB.MinPoolSize = int.Parse(MinPoolSize);
+            pgCSB.MaxPoolSize = int.Parse(MaxPoolSize);
+            pgCSB.ConnectionLifetime = int.Parse(ConnectionLifetime); ;
+
+            pgCSB.ConnectionTimeout = 5;
+            pgCSB.Unicode = true;
+            pgSqlConnection = new PgSqlConnection(pgCSB.ConnectionString);
+            pgSqlConnection.ConnectionLost += new Devart.Common.ConnectionLostEventHandler(pgSqlConnection_ConnectionLost);
+
+        }
+
+        void pgSqlConnection_ConnectionLost(object sender, Devart.Common.ConnectionLostEventArgs e)
+        {
+            Console.WriteLine(DateTime.Now.ToString("F")+"ConnectLost");
+        }
+        public bool connect()
+        {
+            try
+            {
+
+                if (pgSqlConnection != null)
+                {
+                    pgSqlConnection.Open();
+                    IsConnected = true;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (PgSqlException ex)
+            {
+                
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(DateTime.Now.ToString("G") + "-Connect exception occurs: {0}", ex.Error);
+                log.Error("Connect exception occurs: "+ ex.Error);
+                Console.ResetColor();
+                return false;
+            }
+        }
+        public bool disconnect()
+        {
+            try
+            {
+                if (pgSqlConnection != null)
+                {
+                    pgSqlConnection.Close();
+                    IsConnected = false;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (PgSqlException ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Disconnect exception occurs: {0}", ex.Error);
+                log.Error("Disconnect exception occurs: "+ ex.Error);
+                Console.ResetColor();
+                return false;
+            }
+        }
+        //For UPDATE, INSERT, and DELETE statements
+        public bool modify(string cmd)
+        {
+            try
+            {
+                if (pgSqlConnection != null && IsConnected)
+                {
+                    //insert
+                    PgSqlCommand command = pgSqlConnection.CreateCommand();
+                    command.CommandText = cmd;
+                    //cmd.CommandText = "INSERT INTO public.test (id) VALUES (1)";
+
+                    //async
+                    IAsyncResult cres = command.BeginExecuteNonQuery(null, null);
+
+                    if (cres.IsCompleted)
+                        Console.WriteLine("Completed.");
+                    else
+                        Console.WriteLine("Have to wait for operation to complete...");
+                    int RowsAffected = command.EndExecuteNonQuery(cres);
+                    Console.WriteLine("Done. Rows affected: " + RowsAffected.ToString());
+                    /*
+                     //sync
+                     int aff = cmd.ExecuteNonQuery();
+                     Console.WriteLine(aff + " rows were affected.");
+                     * 
+                     */
+                    return true;
+                }
+                else
+                    return false;
+            }
+            catch (PgSqlException ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Modify exception occurs: {0}" + Environment.NewLine + "{1}", ex.Error, cmd);
+                log.Error("Modify exception occurs: " + Environment.NewLine + ex.Error + Environment.NewLine + cmd);
+                Console.ResetColor();
+                return false;
+            }
+
+        }
+        //For SELECT statements
+        public DataTable get_DataTable(string cmd)
+        {
+            try
+            {
+                if (pgSqlConnection != null && IsConnected)
+                {
+                    DataTable datatable = new DataTable();
+                    PgSqlCommand command = pgSqlConnection.CreateCommand();
+                    command.CommandText = cmd;
+                    Console.WriteLine("Starting asynchronous retrieval of data...");
+                    IAsyncResult cres = command.BeginExecuteReader();
+                    if (cres.IsCompleted)
+                        Console.WriteLine("Completed.");
+                    else
+                        Console.WriteLine("Have to wait for operation to complete...");
+                    PgSqlDataReader myReader = command.EndExecuteReader(cres);
+                    try
+                    {
+                        // printing the column names
+                        for (int i = 0; i < myReader.FieldCount; i++)
+                        {
+                            Console.Write(myReader.GetName(i).ToString() + "\t");
+                            datatable.Columns.Add(myReader.GetName(i).ToString(), typeof(string));
+                        }
+                        Console.Write(Environment.NewLine);
+                        while (myReader.Read())
+                        {
+                            DataRow dr = datatable.NewRow();
+
+                            for (int i = 0; i < myReader.FieldCount; i++)
+                            {
+                                Console.Write(myReader.GetString(i) + "\t");
+                                dr[i] = myReader.GetString(i);
+                            }
+                            datatable.Rows.Add(dr);
+                            Console.Write(Environment.NewLine);
+                            //Console.WriteLine(myReader.GetInt32(0) + "\t" + myReader.GetString(1) + "\t");
+                        }
+                    }
+                    finally
+                    {
+                        myReader.Close();
+                    }
+                    foreach (DataRow row in datatable.Rows) // Loop over the rows.
+                    {
+                        Console.WriteLine("--- Row ---"); // Print separator.
+                        foreach (var item in row.ItemArray) // Loop over the items.
+                        {
+                            Console.Write("Item: "); // Print label.
+                            Console.WriteLine(item); // Invokes ToString abstract method.
+                        }
+                    }
+                    return datatable;
+                }
+                else
+                    return null;
+            }
+            catch (PgSqlException ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("GetDataTable exception occurs: {0}"+Environment.NewLine+"{1}", ex.Error,cmd);
+                log.Error("GetDataTable exception occurs: " + Environment.NewLine + ex.Error+Environment.NewLine+ cmd);
+                Console.ResetColor();
+                return null;
+            }
+        }
+
+    }
+}
