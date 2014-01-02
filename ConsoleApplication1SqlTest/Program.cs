106a4f5bbb8c6a7a3f13796dc42d23e645e55140
nb <waertf.register@m2k.com.tw>
2014-01-02 17:51:40 +0800

    add function ShowMessageBoxFromWTSSendMessage

--- ConsoleApplication1SqlTest/Program.cs
+++ ConsoleApplication1SqlTest/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
 using System.Text;
 using System.Threading;
 
@@ -9,8 +11,43 @@ namespace ConsoleApplication1SqlTest
 {
     class Program
     {
+        #region using WTSSendMessage
+        public static IntPtr WTS_CURRENT_SERVER_HANDLE = IntPtr.Zero;
+
+        public static int ShowMessageBoxFromWTSSendMessage(string message, string title, int timeOutInSec, int style)
+        {
+            int resp = 0;
+            WTSSendMessage(
+                WTS_CURRENT_SERVER_HANDLE,
+                WTSGetActiveConsoleSessionId(),
+                title, title.Length,
+                message, message.Length,
+                style, timeOutInSec, out resp, true);
+            return resp;
+        }
+        
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern int WTSGetActiveConsoleSessionId();
+
+        [DllImport("wtsapi32.dll", SetLastError = true)]
+        public static extern bool WTSSendMessage(
+            IntPtr hServer,
+            int SessionId,
+            String pTitle,
+            int TitleLength,
+            String pMessage,
+            int MessageLength,
+            int Style,
+            int Timeout,
+            out int pResponse,
+            bool bWait);
+        #endregion
+
+
         static void Main(string[] args)
         {
+            ShowMessageBoxFromWTSSendMessage("test", "title", 5, 0x30);
             SqlClient sql_client = new SqlClient(ConfigurationManager.AppSettings["SQL_SERVER_IP"], ConfigurationManager.AppSettings["SQL_SERVER_PORT"], ConfigurationManager.AppSettings["SQL_SERVER_USER_ID"], ConfigurationManager.AppSettings["SQL_SERVER_PASSWORD"], ConfigurationManager.AppSettings["SQL_SERVER_DATABASE"], ConfigurationManager.AppSettings["Pooling"], ConfigurationManager.AppSettings["MinPoolSize"], ConfigurationManager.AppSettings["MaxPoolSize"], ConfigurationManager.AppSettings["ConnectionLifetime"]);
             while (!sql_client.connect())
             {
