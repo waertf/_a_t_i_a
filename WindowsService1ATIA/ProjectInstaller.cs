3a92cb9e49d6357d716e0af27afb0c278434776b
nb <waertf.register@m2k.com.tw>
2014-01-03 15:56:09 +0800

    add windows service and setup project

---
+++ WindowsService1ATIA/ProjectInstaller.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+
+
+namespace WindowsService1ATIA
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : Installer
+    {
+        public ProjectInstaller()
+        {
+            InitializeComponent();
+            this.AfterInstall += new InstallEventHandler(ProjectInstaller_AfterInstall);
+        }
+
+        void ProjectInstaller_AfterInstall(object sender, InstallEventArgs e)
+        {
+
+            SetRecoveryOptions(serviceInstaller1.ServiceName);
+
+            using (ServiceController sc = new ServiceController(serviceInstaller1.ServiceName))
+            {
+                sc.Start();
+            }
+        }
+        static void SetRecoveryOptions(string serviceName)
+        {
+            int exitCode;
+            using (var process = new Process())
+            {
+                var startInfo = process.StartInfo;
+                startInfo.FileName = "sc";
+                startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+                // tell Windows that the service should restart if it fails
+                startInfo.Arguments = string.Format("failure \"{0}\" reset= 0 actions= restart/60000", serviceName);
+
+                process.Start();
+                process.WaitForExit();
+
+                exitCode = process.ExitCode;
+
+                process.Close();
+            }
+
+            if (exitCode != 0)
+                throw new InvalidOperationException();
+        }
+    }
+}
